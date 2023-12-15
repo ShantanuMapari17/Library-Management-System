e6651edc48a25c88341cb9d5ff5688f00a19c559
shantanumapari17 <shantanumapari17@gmail.com>
2023-12-15 11:44:53 +0530

    added some comments in views.py

--- library/views.py
+++ library/views.py
@@ -14,7 +14,7 @@ from django.db.models import Q
 
 page = 1
 
-
+#HomePage
 def index(request):
     # return HttpResponse("Hello, world. Welcome to Library Management Service.")
     context={
@@ -59,7 +59,7 @@ def add_books(request):
             n=int(quantity/20)
             #remaining books to parse
             remain=quantity%20
-            #looping to for numbers of pages to get desired number of books
+            # looping to for numbers of pages to get desired number of books
             for i in range(n):
                 #defining params
                 params = {
@@ -92,6 +92,25 @@ def add_books(request):
                 for k in range(remain):
                     book=response[k]
                     books.append(book)
+
+            #for importing specific books needs clarification about statement
+            # while len(books) < quantity:
+            #     params = {
+            #         'title': title,
+            #         'authors': authors,
+            #         'isbn': isbn,
+            #         'publisher': publisher,
+            #         'page':page
+            #     }
+            #     #api call
+            #     api_url = 'https://frappe.io/api/method/frappe-library'
+            #     response = requests.get(api_url,params).json()['message']
+            #     #need to insert each book in books[], so parse response
+            #     for book in response:
+            #         books.append(book)
+            #         if len(books)>=quantity:
+            #             break
+            #     page=(page+1)
                 
             #if no books found then return 
             if len(books)==0:
@@ -120,6 +139,7 @@ def add_books(request):
     context['form']=form
     return render(request,'library/add_books.html',context)
 
+
 def confirm_delete_book(request,pk):
     book = Book.objects.filter(id=pk)
     book.delete()
@@ -133,73 +153,6 @@ def delete_book(request,pk):
     context['book']=book
     return render(request,'library/confirm_delete_book.html',context)
 
-def members_home(request):
-    if request.GET.get('q'):
-        q=request.GET['q']
-    else:
-        q=''
-    members = Members.objects.filter(
-        Q(first_name__icontains = q)|
-        Q(last_name__icontains = q)|
-        Q(email__icontains=q)
-    )
-
-    # members = Members.objects.all()
-    context={
-        'members':members
-    }
-    return render(request,"library/members.html",context)
-
-
-def add_member(request):
-
-    if(request.method=='POST'):
-        form = MemberForm(request.POST)
-        if form.is_valid():
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            email = form.cleaned_data['email']
-            temp = Members.objects.filter(first_name=first_name,last_name=last_name,email=email)
-            if temp:
-                messages.error(request,"User already exists!!")
-                return redirect('add_member')
-
-            member = Members(first_name=first_name,last_name=last_name,email=email)
-            member.save()
-            return redirect('index')
-    else:
-        form = MemberForm()
-    
-    context={}
-    context['form']=form
-    context['name']="shantanu"
-    return render(request,'library/add_members.html',context)
-
-def confirm_delete_member(request,pk):
-    member = Members.objects.get(id=pk)
-    member.delete()
-    messages.success(request,"Deleted Member Successfully")
-    return redirect('index')
-
-def members_detail(request,pk):
-    member = Members.objects.get(id = pk)
-    transactions = Transactions.objects.filter(member=member)
-    books=[]
-    for transaction in transactions:
-        books.append(transaction.book)
-    context={}
-    context['member']=member
-    context['books']=books
-    context['transactions']=transactions
-    return render(request,'library/members_detail.html',context)
-
-
-def delete_member(request,pk):
-    member = Members.objects.get(id=pk)
-    context={}
-    context['member']=member    
-    return render(request,'library/confirm_delete_member.html',context)
-
 def issue_book(request):
     form = BookIssueForm()
     if request.method=="POST":
@@ -210,17 +163,20 @@ def issue_book(request):
             book = transaction.book
             member = transaction.member
             
+            #need to check if user has already taken the same book
             temp = Transactions.objects.filter(book=book, member=member)
             # breakpoint()
             if(temp):
                 messages.error(request,"Can not issue the same book")
                 return redirect('issue_book')
 
+            #check for the debt
             if member.debt>=500:
                 messages.error(request,"Members Debt more than 500 ! Can not issue book")
                 return redirect('issue_book')
             book.quantity=book.quantity-1
 
+            #check for book quantity
             if book.quantity<0:
                 messages.error(request,"Not sufficient Quantity of Book!")
                 return redirect('issue_book')
@@ -277,6 +233,7 @@ def return_book(request):
     context['form']=form
     return render(request,'library/return_book.html',context)
 
+#displaying books details
 def show_book_detail(request,pk):
     book = Book.objects.filter(id=pk)
     
@@ -294,7 +251,78 @@ def show_book_detail(request,pk):
     context['transactions']=temp
     return render(request,'library/book_details.html',context)
 
+#redirecting to members home page
+def members_home(request):
+    if request.GET.get('q'):
+        q=request.GET['q']
+    else:
+        q=''
+    members = Members.objects.filter(
+        Q(first_name__icontains = q)|
+        Q(last_name__icontains = q)|
+        Q(email__icontains=q)
+    )
 
+    # members = Members.objects.all()
+    context={
+        'members':members
+    }
+    return render(request,"library/members.html",context)
+
+#function to add new member
+def add_member(request):
+    if(request.method=='POST'):
+        form = MemberForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+
+            #need to check if member already exists
+            temp = Members.objects.filter(first_name=first_name,last_name=last_name,email=email)
+            if temp:
+                messages.error(request,"User already exists!!")
+                return redirect('add_member')
+
+            member = Members(first_name=first_name,last_name=last_name,email=email)
+            member.save()
+            return redirect('index')
+    else:
+        form = MemberForm()
+    
+    context={}
+    context['form']=form
+    context['name']="shantanu"
+    return render(request,'library/add_members.html',context)
+
+#showing members details
+def members_detail(request,pk):
+    member = Members.objects.get(id = pk)
+    transactions = Transactions.objects.filter(member=member)
+    books=[]
+    for transaction in transactions:
+        books.append(transaction.book)
+    context={}
+    context['member']=member
+    context['books']=books
+    context['transactions']=transactions
+    return render(request,'library/members_detail.html',context)
+
+#function to render a page for confirming delete operation
+def delete_member(request,pk):
+    member = Members.objects.get(id=pk)
+    context={}
+    context['member']=member    
+    return render(request,'library/confirm_delete_member.html',context)
+
+#perform delete operation based on pk
+def confirm_delete_member(request,pk):
+    member = Members.objects.get(id=pk)
+    member.delete()
+    messages.success(request,"Deleted Member Successfully")
+    return redirect('index')
+
+#function to render confirmation form whether to clear debt or not
 def clear_debt(request):
     # breakpoint()
     #get user and redirect it to confirm delete page
@@ -310,6 +338,8 @@ def clear_debt(request):
     context['members']=members
     return render(request,'library/clear_debt.html',context)
 
+
+#after confirmation to delete the debt 
 def confirm_debt_clear(request,pk):
     member = Members.objects.get(id=pk)
     member.debt=0
